3a9d54e96ec1b54e171b41401fed0d600451ba89
Mateusz Furtak <mateusz.furtak@crayon.com>
2026-02-23 23:37:59 +0100

    xx

--- auth.ts
+++ auth.ts
@@ -14,6 +14,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  trustHost: true,
   callbacks: {
     async signIn({ user, account }) {
       if (account?.provider === "google" && user.email) {
